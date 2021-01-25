4375975c673a8fb37dab94780c9bea299628b3e3
Marc-Andre Giroux <xuorig@github.com>
2021-01-25 12:00:24 -0500

    get redocly running

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,63 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+
+import { error, info, setFailed } from '@actions/core';
+
+import { loadConfig, Config, lint, formatProblems } from '@redocly/openapi-core';
+
+import {
+  getExecutionTime,
+  getFallbackEntryPointsOrExit,
+  getTotals,
+  printLintTotals,
+} from '@redocly/openapi-cli/lib/utils';
+
+import { Totals } from '@redocly/openapi-cli/lib/types';
+
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const configFile: string = core.getInput('config_file')
+    const config: Config = await loadConfig(configFile);
+    const entrypoints = await getFallbackEntryPointsOrExit([], config);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const totals: Totals = { errors: 0, warnings: 0, ignored: 0 };
 
-    core.setOutput('time', new Date().toTimeString())
+    for (const entryPoint of entrypoints) {
+      try {
+        const startedAt = performance.now();
+        info(`validating ${entryPoint}...\n`);
+
+        const results = await lint({
+          ref: entryPoint,
+          config,
+        });
+
+        const fileTotals = getTotals(results);
+        totals.errors += fileTotals.errors;
+        totals.warnings += fileTotals.warnings;
+        totals.ignored += fileTotals.ignored;
+
+        formatProblems(results, {
+          format: "codeframe",
+          maxProblems: 100,
+          totals: fileTotals,
+          version: 'TODO',
+        });
+
+        const elapsed = getExecutionTime(startedAt);
+        info(`${entryPoint}: validated in ${elapsed}\n\n`);
+      } catch (e) {
+        error(e.message);
+      }
+    }
+
+    printLintTotals(totals, entrypoints.length);
+
+    if (totals.errors > 0) {
+      setFailed("Lint failed")
+    }
   } catch (error) {
-    core.setFailed(error.message)
+    setFailed(error.message)
   }
 }
 
