63f131fbfd009e0e32d01ccae6dbfbed3706ad88
Marc-Andre Giroux <xuorig@github.com>
2021-01-25 16:24:53 -0500

    basic test

---
+++ __tests__/main.test.ts
@@ -0,0 +1,15 @@
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  process.env['INPUT_CONFIG'] = './fixtures/test_config.yml'
+  process.env['INPUT_ENTRYPOINTS'] = './fixtures/openapi.yml'
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execFileSync(np, [ip], options).toString())
+})
